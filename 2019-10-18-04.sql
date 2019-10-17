6dbc6c088a13610a0c728a7c596607e755b46b24
superj80820 <superj80820@gmail.com>
2019-10-18 04:46:46 +0800

    2019-10-18-04

---
+++ 2019-10-18-04.sql
@@ -0,0 +1,146 @@
+-- MySQL dump 10.13  Distrib 5.7.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: 
+-- ------------------------------------------------------
+-- Server version	5.7.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `djangodocker`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `djangodocker` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+
+USE `djangodocker`;
+
+--
+-- Table structure for table `SequelizeMeta`
+--
+
+DROP TABLE IF EXISTS `SequelizeMeta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `SequelizeMeta` (
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`name`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `SequelizeMeta`
+--
+
+LOCK TABLES `SequelizeMeta` WRITE;
+/*!40000 ALTER TABLE `SequelizeMeta` DISABLE KEYS */;
+INSERT INTO `SequelizeMeta` VALUES ('20191012093852-create-face-info.js'),('20191012101158-create-face-face.js'),('20191012101301-create-face-sub-name.js');
+/*!40000 ALTER TABLE `SequelizeMeta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `faceFaces`
+--
+
+DROP TABLE IF EXISTS `faceFaces`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `faceFaces` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `preview` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `infoId` int(11) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `infoId` (`infoId`),
+  CONSTRAINT `faceFaces_ibfk_1` FOREIGN KEY (`infoId`) REFERENCES `faceInfos` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `faceFaces`
+--
+
+LOCK TABLES `faceFaces` WRITE;
+/*!40000 ALTER TABLE `faceFaces` DISABLE KEYS */;
+/*!40000 ALTER TABLE `faceFaces` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `faceInfos`
+--
+
+DROP TABLE IF EXISTS `faceInfos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `faceInfos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `romanization` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `detail` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `preview` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `faceInfos`
+--
+
+LOCK TABLES `faceInfos` WRITE;
+/*!40000 ALTER TABLE `faceInfos` DISABLE KEYS */;
+INSERT INTO `faceInfos` VALUES (1,'鬆甜美子','akia','cute','https://i.imgur.com/ShqJ1gn.jpg','2019-10-17 17:43:00','2019-10-17 17:43:37'),(2,'aaasdfd','akira','cool','https://i.imgur.com/ieYyB61.jpg','2019-10-17 18:18:43','2019-10-17 18:18:43'),(4,'aaasdfdasdf','akira','cool','https://i.imgur.com/Tmow3GP.jpg','2019-10-17 18:19:32','2019-10-17 18:19:32'),(5,'aaasdfdasdfd','akira','cool','https://i.imgur.com/s54JBwR.jpg','2019-10-17 18:19:40','2019-10-17 18:19:40');
+/*!40000 ALTER TABLE `faceInfos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `faceSubNames`
+--
+
+DROP TABLE IF EXISTS `faceSubNames`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `faceSubNames` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `infoId` int(11) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `infoId` (`infoId`),
+  CONSTRAINT `faceSubNames_ibfk_1` FOREIGN KEY (`infoId`) REFERENCES `faceInfos` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `faceSubNames`
+--
+
+LOCK TABLES `faceSubNames` WRITE;
+/*!40000 ALTER TABLE `faceSubNames` DISABLE KEYS */;
+/*!40000 ALTER TABLE `faceSubNames` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-10-17 20:46:36
